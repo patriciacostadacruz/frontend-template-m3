f9c75f93299b0f6dfdcaf3763f5f62602c32f18b
patriciacostadacruz <patriciacstcz@gmail.com>
2023-03-30 15:35:15 +0200

    Adds index services

---
+++ src/services/indexServices.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+class IndexService {
+  constructor() {
+    this.api = axios.create({
+      baseURL: `${process.env.REACT_APP_BACKEND_URL}/`,
+    });
+
+    this.api.interceptors.request.use((config) => {
+      const storedToken = localStorage.getItem("authToken");
+      if (storedToken) {
+        config.headers = { Authorization: `Bearer ${storedToken}` };
+      }
+      return config;
+    });
+  }
+
+  getRandomProjects() {
+    return this.api.get("/").then(({ data }) => data);
+  }
+}
+
+const indexService = new IndexService();
+
+export default indexService;
