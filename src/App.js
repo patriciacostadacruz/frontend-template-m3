7e6e2c803c75fb32b0ee98487602e857c4c64734
patriciacostadacruz <patriciacstcz@gmail.com>
2023-03-30 12:46:00 +0200

    Fixes app.js - adds new pages names

--- src/App.js
+++ src/App.js
@@ -3,25 +3,22 @@ import { Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import Home from './views/Home';
 import Navbar from './components/Navbar';
-import ErrorPage from './views/ErrorPage';
-import NotFound from './views/NotFound';
+import ErrorContent from "./views/ErrorContent";
+import ErrorPath from "./views/ErrorPath";
 import Signup from './views/auth/Signup';
-import Login from './views/auth/Login';
-import PrivateView from './views/PrivateView';
-import IsPrivate from './components/IsPrivate';
+import Login from "./views/auth/Login";
 
 function App() {
   return (
     <div className="App">
-      <Toaster/>
+      <Toaster />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/private" element={<IsPrivate><PrivateView /></IsPrivate>} />
-        <Route path="/error" element={<ErrorPage />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/error" element={<ErrorContent />} />
+        <Route path="*" element={<ErrorPath />} />
       </Routes>
     </div>
   );
