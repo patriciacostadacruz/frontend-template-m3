9c86bc2d3044cd6fcf96a89f7c9f6c92401a1077
patriciacostadacruz <patriciacstcz@gmail.com>
2023-03-30 15:32:12 +0200

    Adds review services

---
+++ src/services/reviewServices.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+class ReviewService {
+  constructor() {
+    this.api = axios.create({
+      baseURL: `${process.env.REACT_APP_BACKEND_URL}/reviews`,
+    });
+
+    this.api.interceptors.request.use((config) => {
+      const storedToken = localStorage.getItem("authToken");
+      if (storedToken) {
+        config.headers = { Authorization: `Bearer ${storedToken}` };
+      }
+      return config;
+    });
+  }
+
+  addReview(body) {
+    return this.api.post("/new", body).then(({ data }) => data);
+  }
+}
+
+const reviewService = new ReviewService();
+
+export default reviewService;
