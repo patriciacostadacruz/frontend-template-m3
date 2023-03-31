9fb00c01fd6cfaac433bd3ccfa6db801e20e365f
patriciacostadacruz <patriciacstcz@gmail.com>
2023-03-31 19:06:53 +0200

    Adds basic structure for profile

--- src/views/profile/Profile.jsx
+++ src/views/profile/Profile.jsx
@@ -1,5 +1,29 @@
-function Profile({ children }) {
-  return <>My profile</>;
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import profileService from "../../services/profileServices";
+
+function Profile() {
+  const [profileData, setProfileData] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+
+  const getProfile = async () => {
+    try {
+      const response = await profileService.getProfile();
+      setProfileData(response)
+    } catch (error) {
+      setErrorMessage("Sorry, we couldn't retrieve your profile data. Please try again or navigate to another page.");
+    }
+  }
+
+  useEffect(() => {
+    getProfile();
+  }, []);
+
+  return (
+    <>{profileData.email}</>
+  );
 }
 
 export default Profile;
