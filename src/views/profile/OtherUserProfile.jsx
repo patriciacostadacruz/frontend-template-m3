b22bc56dd18e97584649183935fe40dcab19f179
patriciacostadacruz <patriciacstcz@gmail.com>
2023-04-01 12:26:08 +0200

    Adds other user profile get data logic

--- src/views/profile/OtherUserProfile.jsx
+++ src/views/profile/OtherUserProfile.jsx
@@ -1,5 +1,37 @@
-function OtherUserProfile({ children }) {
-  return <>Other user profile</>;
+import { useParams } from "react-router-dom";
+import profileServices from "../../services/profileServices";
+import { useState, useEffect } from "react";
+
+function OtherUserProfile() {
+  const { userId } = useParams();
+  const [otherUser, setOtherUser] = useState(null);
+
+  const getUser = async () => {
+    try {
+      const response = await profileServices.getOtherUser(userId);
+      setOtherUser(response.otherUser);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getUser();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  
+  return (
+    <>
+      {otherUser && (
+        <div>
+          <p>
+            {otherUser.firstName} {otherUser.lastName}
+          </p>
+          <img src={otherUser.image} alt="Avatar"/>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default OtherUserProfile;
