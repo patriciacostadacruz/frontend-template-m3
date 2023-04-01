9f0e374a1a55baa82567cfe4bfed8c04f0c8a79b
patriciacostadacruz <patriciacstcz@gmail.com>
2023-04-01 12:53:49 +0200

    Adds footer structure

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,17 @@
-function Footer({ children }) {
+import { Link } from "react-router-dom";
+
+function Footer() {
   return (
-    <>
-    <p>Footer</p>
-    </>
-  )
+    <div className="footer">
+      <img src="" alt="investMate logo" />
+      <p>Copyright C 2023 investMate, Inc</p>
+      <div className="footer-links">
+        <Link to="">Privacy policy</Link>
+        <Link to="">NDA</Link>
+        <Link to="">Contact</Link>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
