18a0979ec93b1988dcf09f7a0035cb39871563a5
patriciacostadacruz <patriciacstcz@gmail.com>
2023-04-02 17:56:03 +0200

    Few changes done in Dev

--- src/components/project/ProjectData.jsx
+++ src/components/project/ProjectData.jsx
@@ -1,6 +1,48 @@
+import { AuthContext } from "../../context/AuthContext";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+
 function ProjectData({ project }) {
+  const { user } = useContext(AuthContext);
+
   return (
-    <div>
+    <div className="project-detail">
+      <h2>Project detail</h2>
+      {project ? (
+        <div>
+          <div>
+            <h3>{project.title}</h3>
+            <p>
+              <strong>Added by:</strong> {project.owner.firstName}{" "}
+              {project.owner.lastName}
+            </p>
+          </div>
+          <p>
+            <strong>Status:</strong> {project.status}
+          </p>
+          <p>
+            <strong>Location:</strong> {project.location}
+          </p>
+          <p>
+            <strong>Funding needed:</strong> {project.fundingNeeded}
+          </p>
+          <p>
+            <strong>Industry: </strong>
+            {project.industry.map((elem) => {
+              return <span key={project.industry.indexOf(elem)}>{elem}</span>;
+            })}
+          </p>
+          <p>{project.description}</p>
+          <p>
+            <strong>Investors:</strong> {project.investors.length}
+          </p>
+        </div>
+      ) : (
+        "No project to show"
+      )}
+      {/* {user._id !== project.owner._id && (
+        <Link to={``}>Contact {project.owner.firstName}</Link>
+      )} */}
     </div>
   );
 }
--- src/views/projects/Project.jsx
+++ src/views/projects/Project.jsx
@@ -1,14 +1,12 @@
 import { useParams, Link } from "react-router-dom";
-import { useContext } from "react";
 import { useState, useEffect } from "react";
 import projectService from "../../services/projectServices";
-import { AuthContext } from "../../context/AuthContext";
 import toast from "react-hot-toast";
+import ProjectData from "../../components/project/ProjectData";
 
 function Project() {
   const { projectId } = useParams();
   const [project, setProject] = useState(null);
-  const { user } = useContext(AuthContext);
 
   const getProject = async () => {
     try {
@@ -26,44 +24,7 @@ function Project() {
   }, []);
 
   return (
-    <div className="project-detail">
-      <h2>Project detail</h2>
-      {project ? (
-        <div>
-          <div>
-            <h3>{project.title}</h3>
-            <p>
-              <strong>Added by:</strong> {project.owner.firstName}{" "}
-              {project.owner.lastName}
-            </p>
-          </div>
-          <p>
-            <strong>Status:</strong> {project.status}
-          </p>
-          <p>
-            <strong>Location:</strong> {project.location}
-          </p>
-          <p>
-            <strong>Funding needed:</strong> {project.fundingNeeded}
-          </p>
-          <p>
-            <strong>Industry: </strong>
-            {project.industry.map((elem) => {
-              return <span key={project.industry.indexOf(elem)}>{elem}</span>;
-            })}
-          </p>
-          <p>{project.description}</p>
-          <p>
-            <strong>Investors:</strong> {project.investors.length}
-          </p>
-        </div>
-      ) : (
-        "No project to show"
-      )}
-      {user._id !== project.owner._id && (
-        <Link to ={``}>Contact {project.owner.firstName}</Link>
-      )}
-    </div>
+    <ProjectData project={project}/>
   );
 }  
 
