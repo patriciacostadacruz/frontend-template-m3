1a214c455be0fac2448a2d05f655e4024072d74b
patriciacostadacruz <patriciacstcz@gmail.com>
2023-04-03 15:56:31 +0200

    Adds personal and pro darta in user profile

--- src/components/profile/ProfileData.jsx
+++ src/components/profile/ProfileData.jsx
@@ -1,10 +1,54 @@
+import { Link } from "react-router-dom";
+import linkedin from "../../images/linkedin.png"
+
 function ProfileData({ user }) {
+  const handleLinkedIn = () => {
+    window.location.href = `${user.linkedIn}`;
+  }
+
   return (
     <>
-      <h2>{user.firstName} {user.lastName}</h2>
-      <img src={user.image} alt="Avatar" />
-      {user.userProjects ? <p>projects</p> : "You havent added any project yet."}
-      {user.userReviews ? <p>reviews</p> : "You haven't been reviewed by other users yet."}
+      <div>
+        <img height="250" src={user.image} alt="Avatar" />
+        <div>
+          <h2>
+            {user.firstName} {user.lastName}
+          </h2>
+          <p>
+            <strong>Role: </strong>
+            {user.role}
+          </p>
+          {user.linkedIn && (
+            // using anchor to open page in new tab with blank target
+            <a href={user.linkedIn} target="_blank" rel="noreferrer">
+              <img width="30" src={linkedin} alt="Linkedin profile" />
+            </a>
+          )}
+        </div>
+        <div>
+          <p>
+            <strong>Company: </strong>
+            {user.company}
+          </p>
+          <p>
+            <strong>Industry: </strong>
+            {user.industry.map((elem) => {
+              return <span key={user.industry.indexOf(elem)}>{elem}</span>;
+            })}
+          </p>
+          <p>{user.bio}</p>
+        </div>
+      </div>
+      {user.userProjects ? (
+        <p>projects</p>
+      ) : (
+        "You havent added any project yet."
+      )}
+      {user.userReviews ? (
+        <p>reviews</p>
+      ) : (
+        "You haven't been reviewed by other users yet."
+      )}
     </>
   );
 }
