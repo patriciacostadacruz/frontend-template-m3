bfa6d64697e3755b1bdcf847edb8dc5bb2a90ece
patriciacostadacruz <patriciacstcz@gmail.com>
2023-04-03 17:33:44 +0200

    Adds privacy policy and terms and conditions link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,20 @@ function Footer() {
       <img src="" alt="investMate logo" />
       <p>Copyright C 2023 investMate, Inc</p>
       <div className="footer-links">
-        <Link to="">Privacy policy</Link>
-        <Link to="">NDA</Link>
-        <Link to="">Contact</Link>
+        <a
+          href="https://www.termsfeed.com/live/3c01a154-ee1b-4016-8366-6b918e4be0f3"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Terms and conditions
+        </a>
+        <a
+          href="https://www.termsfeed.com/live/97d08cfc-2879-47a7-8959-6ee79f5d7172"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Privacy policy
+        </a>
       </div>
     </div>
   );
