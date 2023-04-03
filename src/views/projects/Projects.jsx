9ba35ab8f1fe02e5e1fa1553a8d98c874ff875bc
patriciacostadacruz <patriciacstcz@gmail.com>
2023-04-03 15:05:18 +0200

    Adds sorting logic in projects view

--- src/views/projects/Projects.jsx
+++ src/views/projects/Projects.jsx
@@ -5,6 +5,8 @@ import toast from "react-hot-toast";
 
 function Projects() {
   const [projects, setProjects] = useState(null);
+  const [sortBy, setSortBy] = useState("title");
+  const [sortDirection, setSortDirection] = useState("title");
 
   const getProjects = async () => {
     try {
@@ -19,11 +21,59 @@ function Projects() {
     getProjects();
   }, []);
 
+  const sortProjects = (projects) => {
+    const sortedProjects = [...projects];
+    if (sortBy === "date") {
+      sortedProjects.sort((a, b) =>
+        sortByDate(a.createdAt, b.createdAt, sortDirection)
+      );
+    } else {
+      sortedProjects.sort((a, b) =>
+        sortByTitle(a.title, b.title, sortDirection)
+      );
+    }
+    return sortedProjects;
+  };
+
+
+  const sortByDate = (a, b, direction) => {
+    const dateA = new Date(a);
+    const dateB = new Date(b);
+    if (direction === "asc") {
+      return dateA - dateB;
+    } else {
+      return dateB - dateA;
+    }
+  };
+
+  const sortByTitle = (a, b, direction) => {
+    if (direction === "asc") {
+      return a.localeCompare(b);
+    } else {
+      return b.localeCompare(a);
+    }
+  };
+
+  const handleSortChange = (e) => {
+    const value = e.target.value.split("_");
+    setSortBy(value[0]);
+    setSortDirection(value[1]);
+  };
+
   return (
     <>
       <h1>Projects</h1>
+      <div>
+      <label>Sort by:</label>
+      <select onChange={handleSortChange} value={`${sortBy}_${sortDirection}`}>
+        <option value="title_asc">Title (A-Z)</option>
+        <option value="title_desc">Title (Z-A)</option>
+        <option value="date_asc">Creation date (oldest first)</option>
+        <option value="date_desc">Creation date (newest first)</option>
+      </select>
+    </div>
       {projects
-        ? projects.map((project) => {
+        ? sortProjects(projects).map((project) => {
             return <ProjectCard project={project} key={project._id} />;
           })
         : "There are no projects to be displayed."}
