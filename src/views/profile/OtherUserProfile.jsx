f2b3370c41564d1b0347b21d257fcd3739b8396e
patriciacostadacruz <patriciacstcz@gmail.com>
2023-04-04 10:57:41 +0200

    Fixes other user profile view failing - jsx issue solved

--- src/views/profile/OtherUserProfile.jsx
+++ src/views/profile/OtherUserProfile.jsx
@@ -25,66 +25,70 @@ function OtherUserProfile() {
   useEffect(() => {
     getUser();
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [userId]);
   
   return (
     <>
       {loading && <Loading />}
-      <div className="profile-data">
-        <img height="250" src={otherUser.image} alt="Avatar" />
-        <div className="profile-data-personal">
-          <div className="profile-name-linkedin">
-            <h2>
-              {otherUser.firstName} {otherUser.lastName}
-            </h2>
-            {otherUser.linkedIn && (
-              // using anchor to open page in new tab with blank target
-              <a href={otherUser.linkedIn} target="_blank" rel="noreferrer">
-                <img width="30" src={linkedin} alt="Linkedin profile" />
-              </a>
+      {otherUser && 
+        <>
+          <div className="profile-data">
+            <img height="250" src={otherUser.image} alt="Avatar" />
+            <div className="profile-data-personal">
+              <div className="profile-name-linkedin">
+                <h2>
+                  {otherUser.firstName} {otherUser.lastName}
+                </h2>
+                {otherUser.linkedIn && (
+                  // using anchor to open page in new tab with blank target
+                  <a href={otherUser.linkedIn} target="_blank" rel="noreferrer">
+                    <img width="30" src={linkedin} alt="Linkedin profile" />
+                  </a>
+                )}
+              </div>
+              <p>
+                <strong>Email: </strong>
+                {otherUser.email}
+              </p>
+              <p>
+                <strong>Role: </strong>
+                {otherUser.role}
+              </p>
+            </div>
+            <div className="profile-data-professional">
+              <p>
+                <strong>Company: </strong>
+                {otherUser.company}
+              </p>
+              <p>
+                <strong>Industry: </strong>
+                {otherUser.industry.map((elem) => {
+                  return <span key={otherUser.industry.indexOf(elem)}>{elem}</span>;
+                })}
+              </p>
+              <p>{otherUser.bio}</p>
+            </div>
+          </div>
+          <button>Send message</button>
+          <button>Rate user</button>
+          <div className="profile-projects">
+            <h3>Projects</h3>
+            {otherUser.userProjects ? (
+              <p>projects</p>
+            ) : (
+              "This user didn't add any project yet."
             )}
           </div>
-          <p>
-            <strong>Email: </strong>
-            {otherUser.email}
-          </p>
-          <p>
-            <strong>Role: </strong>
-            {otherUser.role}
-          </p>
-        </div>
-        <div className="profile-data-professional">
-          <p>
-            <strong>Company: </strong>
-            {otherUser.company}
-          </p>
-          <p>
-            <strong>Industry: </strong>
-            {otherUser.industry.map((elem) => {
-              return <span key={otherUser.industry.indexOf(elem)}>{elem}</span>;
-            })}
-          </p>
-          <p>{otherUser.bio}</p>
-        </div>
-      </div>
-      <button>Send message</button>
-      <button>Rate user</button>
-      <div className="profile-projects">
-        <h3>Projects</h3>
-        {otherUser.userProjects ? (
-          <p>projects</p>
-        ) : (
-          "This user didn't add any project yet."
-        )}
-      </div>
-      <div className="profile-reviews">
-        <h3>Reviews</h3>
-        {otherUser.userReviews ? (
-          <p>reviews</p>
-        ) : (
-          "This user has no ratings no far."
-        )}
-      </div>
+          <div className="profile-reviews">
+            <h3>Reviews</h3>
+            {otherUser.userReviews ? (
+              <p>reviews</p>
+            ) : (
+              "This user has no ratings no far."
+            )}
+          </div>
+        </>  
+      }
       {errorMessage && <p>{errorMessage}</p>}
     </>
   );
