00658f44bc0e7bbbe42273da26ad04e0802a0292
patriciacostadacruz <patriciacstcz@gmail.com>
2023-04-04 10:43:54 +0200

    Applies sorting method to users view - same structure as for projects

--- src/views/Users.jsx
+++ src/views/Users.jsx
@@ -7,6 +7,8 @@ import { useLocation } from "react-router-dom";
 function Users() {
   const [users, setUsers] = useState([]);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [sortBy, setSortBy] = useState("firstName");
+  const [sortDirection, setSortDirection] = useState("asc");
   const [loading, setLoading] = useState(false);
   const { search } = useLocation();
 
@@ -25,20 +27,61 @@ function Users() {
       setErrorMessage("Sorry, we couldn't retrieve users.");
     }
   };
-  
+
   useEffect(() => {
     getUsers();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const sortUsers = (users) => {
+    const sortedUsers = [...users];
+    if (sortBy === "firstName") {
+      sortedUsers.sort((a, b) =>
+        sortByName(a.firstName, b.firstName, sortDirection)
+      );
+    } else if (sortBy === "lastName") {
+      sortedUsers.sort((a, b) =>
+        sortByName(a.lastName, b.lastName, sortDirection)
+      );
+    }
+    return sortedUsers;
+  };
+
+  const sortByName = (a, b, direction) => {
+    if (direction === "asc") {
+      return a.localeCompare(b);
+    } else {
+      return b.localeCompare(a);
+    }
+  };
+
+  const handleSortChange = (e) => {
+    const value = e.target.value.split("_");
+    setSortBy(value[0]);
+    setSortDirection(value[1]);
+  };
 
   return (
     <>
       <h1>Users</h1>
       {loading && <Loading />}
-      {users &&
-        users.map((user) => {
-          return <UserCard user={user} key={user._id} />;
-        })}
+      <div>
+        <label>Sort by:</label>
+        <select
+          onChange={handleSortChange}
+          value={`${sortBy}_${sortDirection}`}
+        >
+          <option value="firstName_asc">First Name (A-Z)</option>
+          <option value="firstName_desc">First Name (Z-A)</option>
+          <option value="lastName_asc">Last Name (A-Z)</option>
+          <option value="lastName_desc">Last Name (Z-A)</option>
+        </select>
+      </div>
+      {users
+        ? sortUsers(users).map((user) => {
+            return <UserCard user={user} key={user._id} />;
+          })
+        : null}
       {errorMessage && <p>{errorMessage}</p>}
     </>
   );
