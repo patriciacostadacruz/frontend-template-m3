d0c467e39c3339d7a88370141edf62cda77adbe5
patriciacostadacruz <patriciacstcz@gmail.com>
2023-04-06 17:09:08 +0200

    Adds user avatar in projectdata view

--- src/components/project/ProjectData.jsx
+++ src/components/project/ProjectData.jsx
@@ -16,57 +16,58 @@ function ProjectData({ project }) {
   return (
     <div className="project-detail">
       <h2>Project detail</h2>
-      {project && 
+      {project && (
+        <div>
           <div>
-            <div>
-              <h3>{project.title}</h3>
-              <p>
-                <strong>Added by:</strong>{" "}
-                {user._id === project.owner._id ? (
-                  <Link to="/profile">
-                    <img src={project.owner.image} style={style} alt="Avatar" />
-                    {project.owner.firstName} {project.owner.lastName}
-                  </Link>
-                ) : (
-                  <Link to={`/profile/${project.owner._id}`}>
-                    {project.owner.firstName} {project.owner.lastName}
-                  </Link>
-                )}
-              </p>
-            </div>
+            <h3>{project.title}</h3>
             <p>
-              <strong>Status:</strong> {project.status}
-            </p>
-            <p>
-              <strong>Location:</strong> {project.location}
-            </p>
-            <p>
-              <strong>Funding needed:</strong> {project.fundingNeeded}
-            </p>
-            <p>
-              <strong>Industry: </strong>
-              {project.industry.map((elem) => {
-                return <span key={project.industry.indexOf(elem)}>{elem}</span>;
-              })}
-            </p>
-            <p>{project.description}</p>
-            <p>
-              <strong>Investors:</strong>
-              {project.investors.length === 0
-                ? "No investors yet."
-                : project.investors.map((person) => {
-                    return (
-                      <img
-                        key={`${project._id}${project.investors.length}`}
-                        height="40"
-                        src={peopleCount}
-                        alt="Investors count"
-                      />
-                    );
-                  })}
+              <strong>Added by:</strong>{" "}
+              {user._id === project.owner._id ? (
+                <Link to="/profile">
+                  <img src={project.owner.image} style={style} alt="Avatar" />
+                  {project.owner.firstName} {project.owner.lastName}
+                </Link>
+              ) : (
+                <Link to={`/profile/${project.owner._id}`}>
+                  <img src={project.owner.image} style={style} alt="Avatar" />
+                  {project.owner.firstName} {project.owner.lastName}
+                </Link>
+              )}
             </p>
           </div>
-        }
+          <p>
+            <strong>Status:</strong> {project.status}
+          </p>
+          <p>
+            <strong>Location:</strong> {project.location}
+          </p>
+          <p>
+            <strong>Funding needed:</strong> {project.fundingNeeded}
+          </p>
+          <p>
+            <strong>Industry: </strong>
+            {project.industry.map((elem) => {
+              return <span key={project.industry.indexOf(elem)}>{elem}</span>;
+            })}
+          </p>
+          <p>{project.description}</p>
+          <p>
+            <strong>Investors:</strong>
+            {project.investors.length === 0
+              ? "No investors yet."
+              : project.investors.map((person) => {
+                  return (
+                    <img
+                      key={`${project._id}${project.investors.length}`}
+                      height="40"
+                      src={peopleCount}
+                      alt="Investors count"
+                    />
+                  );
+                })}
+          </p>
+        </div>
+      )}
     </div>
   );
 }
