487fb0ae550c7e5d745948b8ca0d1a5ed709079e
patriciacostadacruz <patriciacstcz@gmail.com>
2023-04-06 14:19:27 +0200

    Adds restructuring in messenger routes

--- src/services/messengerServices.js
+++ src/services/messengerServices.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class MessengerService {
   constructor() {
     this.api = axios.create({
-      baseURL: `${process.env.REACT_APP_BACKEND_URL}/conversations`,
+      baseURL: `${process.env.REACT_APP_BACKEND_URL}/`,
     });
 
     this.api.interceptors.request.use((config) => {
@@ -17,49 +17,42 @@ class MessengerService {
 
   getConversations() {
     return this.api
-      .get("/")
+      .get("/conversations")
       .then(({ data }) => data)
       .catch(({ error }) => error);
   }
 
-  createConversation(body) {
+  createConversation(recipientId, users) {
     return this.api
-      .post("/", body)
+      .post(`/conversations/${recipientId}`, users)
       .then(({ data }) => data)
       .catch(({ error }) => error);
   }
 
-  getConversation(conversationId) {
+  getConvMessages(conversationId) {
     return this.api
-      .get(`/${conversationId}`)
+      .get(`/messages/${conversationId}`)
       .then(({ data }) => data)
       .catch(({ error }) => error);
   }
 
   sendMessage(conversationId, body) {
     return this.api
-      .post(`/${conversationId}`, body)
+      .post(`/messages/${conversationId}`, body)
       .then(({ data }) => data)
       .catch(({ error }) => error);
   }
 
   editMessage(messageId, body) {
     return this.api
-      .put(`/${messageId}`, body)
+      .put(`/messages/${messageId}`, body)
       .then(({ data }) => data)
       .catch(({ error }) => error);
   }
 
   deleteMessage(messageId) {
     return this.api
-      .delete(`/${messageId}`)
-      .then(({ data }) => data)
-      .catch(({ error }) => error);
-  }
-
-  deleteConversation(conversationId) {
-    return this.api
-      .delete(`/${conversationId}`)
+      .delete(`/messages/${messageId}`)
       .then(({ data }) => data)
       .catch(({ error }) => error);
   }
