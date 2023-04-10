92db771018cde798076ae28ec4aa83d89b2ed3d5
patriciacostadacruz <patriciacstcz@gmail.com>
2023-04-10 13:25:25 +0200

    Restructures messages and fixes deletion issue where component didn't re-render after deleting

--- src/components/messenger/ConvMessages.jsx
+++ src/components/messenger/ConvMessages.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useContext, useRef } from "react";
+import { useParams } from "react-router-dom";
 import messengerServices from "../../services/messengerServices";
 import { AuthContext } from "../../context/AuthContext";
 import Message from "./Message";
@@ -6,8 +7,63 @@ import toast from "react-hot-toast";
 
 function ConvMessages({ messagesFromFather }) {
   const [messages, setMessages] = useState(null);
+  const [newMessage, setNewMessage] = useState("");
   const messagesEndRef = useRef(null);
+  const inputRef = useRef(null);
+  const { conversationId } = useParams();
   const { user } = useContext(AuthContext);
+
+  const getMessages = async () => {
+    try {
+      const response = await messengerServices.getConvMessages(conversationId);
+      if (response.error) {
+        toast.error(response.error);
+      } else {
+        setMessages(response.messages);
+      }
+    } catch (error) {
+      toast.error("Failed to fetch messages.");
+    }
+  };
+
+  const handleNewMessageChange = (e) => {
+    setNewMessage(e.target.value);
+  };
+
+  const handleKeyDown = (e) => {
+    if (
+      e.key === "Enter" &&
+      inputRef.current &&
+      // allows to send with enter button only if user has input elected
+      inputRef.current === document.activeElement
+    ) {
+      handleSendMessage();
+    }
+  };
+
+  const handleSendMessage = async () => {
+    if (!newMessage) {
+      toast.error("Please write your message content.");
+      return;
+    }
+    try {
+      const response = await messengerServices.sendMessage(conversationId, {
+        recipient:
+          messages[0].sender._id === user._id
+            ? messages[0].recipient._id
+            : messages[0].sender._id,
+        content: newMessage,
+      });
+      if (response.error) {
+        toast.error(response.error);
+      } else {
+        setMessages([...messages, response.message]);
+        setNewMessage("");
+      }
+    } catch (error) {
+      toast.error("Failed to send message.");
+    }
+  };
 
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
@@ -50,15 +106,24 @@ function ConvMessages({ messagesFromFather }) {
   };
 
   useEffect(() => {
-    scrollToBottom();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [messages]);
+    getMessages();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     getMessages();
+  //   }, 2000);
+  //   scrollToBottom();
+  //   return () => clearInterval(interval);
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [messages]);
 
   return (
     <>
-      {messagesFromFather && messagesFromFather.length > 0 ? (
+      {messages && messages.length > 0 ? (
         <div>
-          {messagesFromFather.map((message) => (
+          {messages.map((message) => (
             <Message
               key={message._id}
               message={message}
@@ -72,6 +137,14 @@ function ConvMessages({ messagesFromFather }) {
       ) : (
         "No messages to show."
       )}
+      <input
+        type="text"
+        placeholder="Type your message here."
+        value={newMessage}
+        onChange={handleNewMessageChange}
+        onKeyDown={handleKeyDown}
+        ref={inputRef}
+      />
     </>
   );
 }
--- src/views/messenger/ConvContainer.jsx
+++ src/views/messenger/ConvContainer.jsx
@@ -1,109 +1,31 @@
 import { useState, useContext, useRef, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import messengerServices from "../../services/messengerServices";
-import profileServices from "../../services/profileServices";
 import { AuthContext } from "../../context/AuthContext";
 import ConvMessages from "../../components/messenger/ConvMessages";
-import toast from "react-hot-toast";
 
 function ConvContainer() {
-  const [messages, setMessages] = useState(null);
-  const [newMessage, setNewMessage] = useState("");
   const [otherUser, setOtherUser] = useState(null);
-  const inputRef = useRef(null);
-  const { conversationId } = useParams();
   const { user } = useContext(AuthContext);
-
-  const getMessages = async () => {
-    try {
-      const response = await messengerServices.getConvMessages(conversationId);
-      if (response.error) {
-        toast.error(response.error);
-      } else {
-        setMessages(response.messages);
-      }
-    } catch (error) {
-      toast.error("Failed to fetch messages.");
-    }
-  };
 
   const getOtherUser = async () => {
     let otherUserId;
-    // get the other user ID by returning the one not equal to logged user ID
-    if (messages.length > 0) {
-      const firstMessage = messages[0];
-      if (firstMessage.sender._id === user._id) {
-        otherUserId = firstMessage.recipient._id;
-      } else {
-        otherUserId = firstMessage.sender._id;
-      }
-    }
-    if (otherUserId) {
-      try {
-        const response = await profileServices.getOtherUser(otherUserId);
-        setOtherUser(response.otherUser);
-      } catch (error) {
-        toast.error("Failed to get other user data.");
-      }
-    }
-  };
-
-  const handleNewMessageChange = (e) => {
-    setNewMessage(e.target.value);
-  };
-
-  const handleKeyDown = (e) => {
-    if (
-      e.key === "Enter" &&
-      inputRef.current &&
-      // allows to send with enter button only if user has input elected
-      inputRef.current === document.activeElement
-    ) {
-      handleSendMessage();
-    }
-  };
-
-  const handleSendMessage = async () => {
-    if (!newMessage) {
-      toast.error("Please write your message content.");
-      return;
-    }
     try {
-      const response = await messengerServices.sendMessage(conversationId, {
-        recipient:
-          messages[0].sender._id === user._id
-            ? messages[0].recipient._id
-            : messages[0].sender._id,
-        content: newMessage,
-      });
-      if (response.error) {
-        toast.error(response.error);
-      } else {
-        setMessages([...messages, response.message]);
-        setNewMessage("");
-      }
+      //
     } catch (error) {
-      toast.error("Failed to send message.");
+      //
     }
   };
 
   useEffect(() => {
-    getMessages();
     getOtherUser();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     getMessages();
-  //   }, 2000);
-  //   return () => clearInterval(interval);
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [messages]);
+  
 
   return (
     <>
-      {messages ? (
+      {/* {messages ? (
         <h2>
           {otherUser && (
             <Link to={`/profile/${otherUser._id}`}>
@@ -113,16 +35,8 @@ function ConvContainer() {
         </h2>
       ) : (
         <p>New conversation</p>
-      )}
-      {messages && <ConvMessages messagesFromFather={messages} />}
-      <input
-        type="text"
-        placeholder="Type your message here."
-        value={newMessage}
-        onChange={handleNewMessageChange}
-        onKeyDown={handleKeyDown}
-        ref={inputRef}
-      />
+      )} */}
+      <ConvMessages />
     </>
   );
 }
